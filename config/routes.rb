a10445c389567e1cf5dbcbec00f1ed56b4a8a0de
alXmas <alexsandr.xmas@gmail.com>
2018-12-28 16:56:12 +0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  namespace :web do
-    get 'sessions/new'
+  scope module: :web do
+    resource :board, only: :show
+    resource :session, only: :new
   end
-  namespace :web do
-    get 'board/show'
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
