056507c3d7bb658136804798f234cf139d633a32
alXmas <alexsandr.xmas@gmail.com>
2019-01-28 15:14:04 +0400

    Add seeds

--- test/controllers/web/board_controller_test.rb
+++ test/controllers/web/board_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class Web::BoardControllerTest < ActionDispatch::IntegrationTest
+  setup do user = create(:user)
+  sign_in_as user
+  end
+
   test "should get show" do
     get board_url
     assert_response :success
