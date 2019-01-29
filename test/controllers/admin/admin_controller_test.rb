b7da5a88fc74a7a1a848bc2f8630ec15d179092e
alXmas <alexsandr.xmas@gmail.com>
2019-01-29 09:26:33 +0400

    Fix PR

--- test/controllers/admin/admin_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    user = create(:user)
-    get admin_user_url(user)
-    assert_response :success
-  end
-end
--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -5,38 +5,45 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     admin = create(:admin)
     sign_in_as admin
   end
-   test "should post create" do
-    user = attributes_for(:user)
-    post admin_users_url, params: { user: user }
+
+  test "should get index" do
+    get admin_users_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    user = create(:user, { password: generate(:string) })
+    get admin_user_url user.id
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_admin_user_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    user = create(:user, { password: generate(:string) })
+    get edit_admin_user_url user.id
+    assert_response :success
+  end
+
+  test "should patch update" do
+    user = create(:user)
+    attrs = attributes_for(:user)
+    patch admin_user_url user.id, params: { user: attrs }
     assert_response :redirect
   end
-   test "should delete destroy" do
+
+  test "should delete destroy" do
     user = create(:user)
     delete admin_user_url user.id
     assert_response :redirect
   end
-   test "should get edit" do
-    user = create(:user)
-    get edit_admin_user_url user.id
-    assert_response :success
-  end
-   test "should get index" do
-    get admin_users_url
-    assert_response :success
-  end
-   test "should get new" do
-    get new_admin_user_url
-    assert_response :success
-  end
-   test "should get show" do
-    user = create(:user)
-    get admin_users_url
-    assert_response :success
-  end
-   test "should patch update" do
-    user = create(:user)
-    user_attrs = attributes_for(:user)
-    patch admin_user_url user.id, params: { user: user_attrs }
+
+  test "should post create" do
+    attrs = attributes_for(:user)
+    post admin_users_url, params: { user: attrs }
     assert_response :redirect
   end
 end
