48508b0c682fc312170789d5df989d58305c8f4b
alXmas <alexsandr.xmas@gmail.com>
2019-02-08 13:46:59 +0400

    add api

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     name { generate :string }
     description { generate :string }
-    author_id { "" }
-    assignee_id { "" }
+    author_id { create :user }
+    assignee_id { create :user }
   end
 end
